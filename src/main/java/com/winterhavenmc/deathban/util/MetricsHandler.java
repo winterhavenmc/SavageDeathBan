9c678f87dd2cca1f2696c0fb8d7e3fb632ff970a
Tim Savage <tim.savage@gmail.com>
2022-03-05 15:33:14 -0600

    add metrics chart

--- src/main/java/com/winterhavenmc/deathban/util/MetricsHandler.java
+++ src/main/java/com/winterhavenmc/deathban/util/MetricsHandler.java
@@ -11,6 +11,8 @@ public class MetricsHandler {
 
 		Metrics metrics = new Metrics(plugin, 14522);
 
+		metrics.addCustomChart(new SimplePie("ban_ip", () -> String.valueOf(plugin.getConfig().getString("ban-ip"))));
+
 		metrics.addCustomChart(new SimplePie("ban_time", () -> String.valueOf(plugin.getConfig().getString("ban-time"))));
 
 		metrics.addCustomChart(new SimplePie("kick_delay", () -> String.valueOf(plugin.getConfig().getString("kick-delay"))));
