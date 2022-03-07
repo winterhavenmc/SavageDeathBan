5f4bcb139957f60344f5c0108eec8ba77edc780a
Tim Savage <tim.savage@gmail.com>
2022-03-07 16:39:32 -0600

    remove redundant string conversions

--- src/main/java/com/winterhavenmc/deathban/util/MetricsHandler.java
+++ src/main/java/com/winterhavenmc/deathban/util/MetricsHandler.java
@@ -4,9 +4,6 @@ import com.winterhavenmc.deathban.PluginMain;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
 
-import static com.winterhavenmc.util.TimeUnit.MINUTES;
-import static com.winterhavenmc.util.TimeUnit.SECONDS;
-
 
 public class MetricsHandler {
 
@@ -14,13 +11,13 @@ public class MetricsHandler {
 
 		Metrics metrics = new Metrics(plugin, 14522);
 
-		metrics.addCustomChart(new SimplePie("language", () -> String.valueOf(plugin.getConfig().getString("language"))));
+		metrics.addCustomChart(new SimplePie("language", () -> plugin.getConfig().getString("language")));
 
-		metrics.addCustomChart(new SimplePie("ban_ip", () -> String.valueOf(plugin.getConfig().getString("ban-ip"))));
+		metrics.addCustomChart(new SimplePie("ban_ip", () -> plugin.getConfig().getString("ban-ip")));
 
-		metrics.addCustomChart(new SimplePie("ban_time", () -> plugin.messageBuilder.getTimeString(MINUTES.toMillis(plugin.getConfig().getLong("ban-time")))));
+		metrics.addCustomChart(new SimplePie("ban_time", () -> plugin.getConfig().getString("ban-time")));
 
-		metrics.addCustomChart(new SimplePie("kick_delay", () -> plugin.messageBuilder.getTimeString(SECONDS.toMillis(plugin.getConfig().getLong("kick-delay")))));
+		metrics.addCustomChart(new SimplePie("kick_delay", () -> plugin.getConfig().getString("kick-delay")));
 
 	}
 
