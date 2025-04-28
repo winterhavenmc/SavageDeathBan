94fe2309e8bc137b2d65eb14686376131a9649ad
Tim Savage <tim.savage@gmail.com>
2025-04-28 06:00:28 -0500

    edit for style

--- src/main/java/com/winterhavenmc/deathban/PluginMain.java
+++ src/main/java/com/winterhavenmc/deathban/PluginMain.java
@@ -19,8 +19,6 @@ package com.winterhavenmc.deathban;
 
 import com.winterhavenmc.deathban.commands.CommandManager;
 import com.winterhavenmc.deathban.eventhandlers.PlayerEventHandler;
-import com.winterhavenmc.deathban.messages.Macro;
-import com.winterhavenmc.deathban.messages.MessageId;
 import com.winterhavenmc.deathban.util.MetricsHandler;
 
 import com.winterhavenmc.library.messagebuilder.MessageBuilder;
@@ -28,33 +26,24 @@ import com.winterhavenmc.util.soundconfig.SoundConfiguration;
 import com.winterhavenmc.util.soundconfig.YamlSoundConfiguration;
 import com.winterhavenmc.util.worldmanager.WorldManager;
 
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.JavaPluginLoader;
-
-import java.io.File;
 
 
-public class PluginMain extends JavaPlugin {
-
-	// message builder
-	public MessageBuilder<MessageId, Macro> messageBuilder;
-
-	// sound configuration
+public class PluginMain extends JavaPlugin
+{
+	public MessageBuilder messageBuilder;
 	public SoundConfiguration soundConfig;
-
-	// world manager
 	public WorldManager worldManager;
 
 
 	@Override
-	public void onEnable() {
-
+	public void onEnable()
+	{
 		// install default config.yml if not present
 		saveDefaultConfig();
 
 		// instantiate message builder
-		messageBuilder = new MessageBuilder<>(this);
+		messageBuilder = MessageBuilder.create(this);
 
 		// instantiate sound configuration
 		soundConfig = new YamlSoundConfiguration(this);
